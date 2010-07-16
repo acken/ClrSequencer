861d9c09c7b83e16a0dd6979bc9e2ef621474935
Svein Arne Ackenhausen <acken@acken.no>
2010-07-17 00:40:34 +0200

    Inital commit

---
+++ src/ClrSequencer.Core.Test/Debugger/ClrProcessTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace ClrSequencer.Core.Test.Debugger
+{
+    [TestFixture]
+    public class ClrProcessTest
+    {
+
+    }
+}
---
+++ src/ClrSequencer.Core/Debugger/ClrProcess.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClrSequencer.Core.Debugger
+{
+    class ClrProcess
+    {
+    }
+}
